d7bdf8d528bb0ccb9e973d66e2ad2d2b50013cb7
Aashish Sharma <aashish1502@gmail.com>
2017-04-08 18:25:52 +0530

    Custom styled navigation bar

--- public/component/Navbar.jsx
+++ public/component/Navbar.jsx
@@ -7,14 +7,14 @@ var Navbar = React.createClass({
         <div className="top-bar">
           <div className="top-bar-left">
             <ul className="dropdown menu" data-dropdown-menu>
-            <li className="menu-text"><Link to="/">React Timer</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/examples">Examples</Link></li>
+              <li className="menu-text"><Link to="/">React Timer</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/examples">Examples</Link></li>
             </ul>
           </div>
           <div className="top-bar-right">
             <ul className="menu">
-              <li className="menu-text"><a href="https://github.com/FLASHGORDON"><strong>Aashish Sharma</strong></a></li>
+              <li className="menu-text">Created By<a href="https://github.com/FLASHGORDON"><strong>Aashish Sharma</strong></a></li>
             </ul>
           </div>
         </div>
